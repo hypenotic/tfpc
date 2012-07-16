27ea01bec2a5adc336b15e9114aea344d2f86304
Collin Henderson <hendo13@me.com>
2012-07-16 11:52:40 -0400

    Resources template

---
+++ wordpress/wp-content/themes/modernizm/page-resources.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Resources
+*/
+ ?>
+ <?php get_header();?>
+ <div id="content" class="recent-list columns col12">
+ 	<?php while ( have_posts() ) : the_post(); ?>
+ 	<?php the_content();?>
+ 	<?php endwhile; ?>
+ 	<div class="recent-entry columns col12">
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 		<div class="column col2"><h4 class="entry-title">Main Category</h4></div>
+ 	</div>	
+ </div>
