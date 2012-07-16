c90c39a0116bad36ea6853052467a6e9642767fc
Collin Henderson <hendo13@me.com>
2012-07-16 13:25:29 -0400

    Resources Template Update

--- wordpress/wp-content/themes/modernizm/page-resources.php
+++ wordpress/wp-content/themes/modernizm/page-resources.php
@@ -11,31 +11,32 @@ Template Name: Resources
  	</div>
  	<?php endwhile; ?>
  	<div class="recent-entry columns col12">
- 		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+ 		<div class="recent-list columns">
+	 		<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
+			<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
+			<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
+			<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
+			<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
+			<div class="column col2">
+	 			<h4 class="entry-title">Main Category</h4>
+	 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
+			</div>
 		</div>
-		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
-		</div>
-		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
-		</div>
-		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
-		</div>
-		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
-		</div>
-		<div class="column col2">
- 			<h4 class="entry-title">Main Category</h4>
- 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qui ita affectus, beatum esse numquam probabis; Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Duo Reges: constructio interrete. Non laboro, inquit, de nomine. </p>
-		</div>
- 		
  	</div>	
  </div>
  <?php get_footer(); ?>
