8ed92f3dd97f67cc0ecccbf41ad7958e109d4cea
Collin Henderson <hendo13@me.com>
2012-07-18 11:09:51 -0400

    New template

--- wordpress/wp-content/themes/modernizm/single.php
+++ wordpress/wp-content/themes/modernizm/single.php
@@ -37,6 +37,7 @@
 					$category = get_the_category(); 
 					if($category[0]){
 						echo 'Filed Under: <a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
+					}
 					?>
 				</div>			
 				<?php the_content(); ?>
