74e5ade8227234c0b28f870b24120d5504047922
Collin Henderson <hendo13@me.com>
2012-07-18 12:53:44 -0400

    Resources page updates

--- wordpress/wp-content/themes/modernizm/archive.php
+++ wordpress/wp-content/themes/modernizm/archive.php
@@ -2,11 +2,36 @@
 
 <div id="content" class="recent-list columns col10">
 <div class="column col10">
- <h3><?php dynamictitles();?></h3>
- <?php 
- $cat_id =  get_query_var('cat');
- echo '<p>'.get_cat_name($cat_id).'</p>';
- ?>
+	<?php
+	$cat_id =  get_query_var('cat');
+	if(get_cat_name($cat_id) == "Introduction"){
+		echo '<h3>Introduction</h3>';
+		echo '<p>Food security, food sovereignty and the history of food movements and networks in the GTA.</p>'
+	}
+ 	elseif(get_cat_name($cat_id) == "Availability"){
+ 		echo '<h3>Availability</h3>';
+		echo '<p>Food production, processing and distribution.</p>'
+	}
+	elseif(get_cat_name($cat_id) == "Accessibility"){
+		echo '<h3>Accessibility</h3>';
+		echo '<p>The changing food environment: purchasing, procurement, income and inclusion.</p>'
+	}
+	elseif(get_cat_name($cat_id) == "Acceptability"){
+		echo '<h3>Acceptability</h3>';
+		echo '<p>Diversity leading change.</p>'
+	}
+	elseif(get_cat_name($cat_id) == "Adequacy"){
+		echo '<h3>Adequacy</h3>';
+		echo '<p>Envisioning a sustainable food system.</p>'
+	}
+	elseif(get_cat_name($cat_id) == "Agency"){
+		echo '<h3>Agency</h3>';
+		echo '<p>Taking action for a better food system.</p>'
+	}
+ 	else {
+		echo '<h3>'.dynamictitles().'</h3>';
+	}
+	?>
 </div>
 <?php if ( have_posts() ) {?>
 <?php
