c89fc4db382a0a0bd41f6d04074f4bd475a01004
Hubert <hubert@hypenotic.com>
2020-01-21 13:18:06 -0500

    Update functions.php

--- wordpress/wp-content/themes/modernizm/functions.php
+++ wordpress/wp-content/themes/modernizm/functions.php
@@ -29,4 +29,47 @@ require_once ($includes_path . 'theme-widgets.php');		// Theme widgets
 if ( ! isset( $content_width ) )
 	$content_width = 600;
 
+function wpse_287931_register_categories_names_field() {
+	register_rest_field( 'project',
+		'categories_names',
+		array(
+			'get_callback'    => 'wpse_287931_get_categories_names',
+			'update_callback' => null,
+			'schema'          => null,
+		)
+	);
+}
+
+add_action( 'rest_api_init', 'wpse_287931_register_categories_names_field' );
+
+function wpse_287931_get_categories_names( $object, $field_name, $request ) {
+
+	$formatted_categories = array();
+	$categories = get_the_category( $object['id'] );
+	foreach ($categories as $category) {
+		$formatted_categories[] = $category->name;
+	}
+	return $formatted_categories;
+}
+
+function add_taxonomies_to_pages() {
+	register_taxonomy_for_object_type('post_tag', 'page');
+	register_taxonomy_for_object_type('category', 'page');
+}
+add_action('init', 'add_taxonomies_to_pages');
+
+if (!is_admin()) {
+	add_action('pre_get_posts', 'category_and_tag_archives');
+}
+
+function category_and_tag_archives($wp_query) {
+	$my_post_array = array('post', 'page');
+
+	if ($wp_query - > get('category_name') || $wp_query - > get('cat'))
+		$wp_query - > set('post_type', $my_post_array);
+
+	if ($wp_query - > get('tag'))
+		$wp_query - > set('post_type', $my_post_array);
+}
+
 ?>
