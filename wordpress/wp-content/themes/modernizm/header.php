390524f37066ffe5ba9add43ce0e0a2ed35ea264
lionelmann <lionelmann@gmail.com>
2020-07-24 10:13:28 -0400

    revert

--- wordpress/wp-content/themes/modernizm/header.php
+++ wordpress/wp-content/themes/modernizm/header.php
@@ -16,7 +16,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_directory' ); ?>/includes/css/flexslider.css">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_directory' ); ?>/includes/css/jquery.jtweetsanywhere.css">
 
-<?php //if ( function_exists( 'colabs_head') ) colabs_head(); ?>
+<?php if ( function_exists( 'colabs_head') ) colabs_head(); ?>
 <?php wp_head(); ?>
 </head>
 
